03d7ec7d66a8a0b849d9c2a17384a46d0929da77
Júlio <tdsjsa2010@gmail.com>
2026-05-07 12:32:50 -0300

    Add files via upload

--- portaria/script.js
+++ portaria/script.js
@@ -5,12 +5,24 @@ function check(){
 
 
     if(isNaN(wasborn)){
-        res.textContent = 'type only numbers dumbass 🚫'
+        res.textContent = 'type only numbers dumb bitch 🚫'
         res.style.color = 'red'
         return
     }
 
     const thisYear = new Date().getFullYear()
     const age = thisYear - wasborn
+
+    if(age >= 18){
+        res.textContent = "Over 18, you can come in"
+        clientAge.textContent = age
+        document.getElementById('v1').style.display = "block"
+        document.getElementById('v2').style.display = "none"
+    }else{
+        res.textContent = "You are under 18, GET THE FUCK OUT 🚫🚫"
+        clientAge.textContent = age
+        document.getElementById('v1').style.display = "none"
+        document.getElementById('v2').style.display = "block"
+    }
 
 }
